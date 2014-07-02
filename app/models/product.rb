8ce329e71808d6bad3052ad8b5d840ec0bb8158d
vitaha1978 <vitaliy.parshyn@gmail.com>
2014-07-02 20:20:45 +0300

    Add button contacts

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ActiveRecord::Base
 
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
@@ -11,4 +14,15 @@ class Product < ActiveRecord::Base
   def to_param
     "#{id}-#{title}"
   end
+
+  private
+    # убеждаемся в отсутствии товарных позиций, ссылающихся на данный товар
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Існують товарні позиції які ссилаються на даний товар')
+        return false
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'store/index'
 
+  match '/contacts', to: 'static_pages#contacts', via: 'get'
+
+  resources :carts
   resources :products
 
+  get 'store/index'
   root to: 'store#index', as: 'store'
 end
