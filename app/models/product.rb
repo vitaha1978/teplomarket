b907ed7a3d9b48948e806e78b903908709166a5f
vitaha1978 <vitaliy.parshyn@gmail.com>
2014-07-02 14:44:42 +0300

    Test stand for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,13 @@
 class Product < ActiveRecord::Base
 
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+            with: %r{\.(gif|jpg|png|jpeg)\Z}i,
+            message: 'повинен бути у форматі GIF, JPG, JPEG або PNG.'
+            }
+
   def to_param
     "#{id}-#{title}"
   end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,67 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def new_product(image_url)
+    Product.new(title: "My title",
+                description: "My description",
+                price: 1,
+                image_url: image_url)
+  end
+
+  test "Image_url" do
+    # URL зображення
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+              http://a.s.c/x/y/x/frd.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} не повинно бути невірним"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} не повинно бути вірним"
+    end
+  end
+
+  test "Products atributes must not be empty" do
+    # Властивості товару не повинні бути пустими
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "Rroduct price must be positive" do
+    product = Product.new(title:       "My Book Title",
+                          description: "yyy",
+                          image_url:   "zzz.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  test "Product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title, 
+                          description: "My description",
+                          price: 1,
+                          image_url: "fred.png")
+    assert product.invalid?
+
+    assert_equal [I18n.translate('errors.messages.taken')], 
+                  product.errors[:title]
+
+  end
+
 end
