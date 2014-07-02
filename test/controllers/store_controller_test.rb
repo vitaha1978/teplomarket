6d453b32fe3f986758d00b9a9f9b797d1884f152
vitaha1978 <vitaliy.parshyn@gmail.com>
2014-07-02 18:35:02 +0300

    Before cart

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,9 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '.entry .price', minimum: 3
+    assert_select 'h1', 1
+    assert_select '.price', /[,\d]+\.\d\d/
   end
 
 end
