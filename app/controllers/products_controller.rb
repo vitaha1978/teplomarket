f839012ca4ccc68460fab703736651f8a496900e
vitaha1978 <info@teplomarket.com.ua>
2014-07-05 13:27:47 +0300

    Password for products and articles

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  http_basic_authenticate_with name: "vitaha", password: "vitaha1978", except: [:index, :show]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   # GET /products
