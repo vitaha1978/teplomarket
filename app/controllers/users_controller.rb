040fb05201490602271a68d547fa377457c208a1
vitaha1978 <info@teplomarket.com.ua>
2014-07-29 07:09:32 +0000

    User controller add new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,20 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
-      
+      redirect_to @user
+      flash[:success] = "Ласкаво просимо на сайт ТеплоМаркет!"
     else
       render 'new'
     end
   end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                     :password_confirmation)
+    end
+  
 end
