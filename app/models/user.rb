a0027d5f642b1f94825c86e37946ffc8aaa7e078
vitaha1978 <info@teplomarket.com.ua>
2014-07-29 10:46:50 +0000

    Sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,27 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+  
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true
   validates :password, length: { minimum: 6 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+                    uniqueness: { case_sensitive: false }  
   
-  before_save { self.email = email.downcase }
   has_secure_password
+  
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140724125725) do
+ActiveRecord::Schema.define(version: 20140729101902) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -87,8 +87,10 @@ ActiveRecord::Schema.define(version: 20140724125725) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
